19f9c2541931f0aa05a677ab88228271968cf50b
Gary Ash <gary.ash@icloud.com>
2026-02-23 16:35:49 -0500

    [TIDY]: Omit days from uptime display when days is zero

--- Sources/startup-banner/DataGatherers/UptimeInfo.swift
+++ Sources/startup-banner/DataGatherers/UptimeInfo.swift
@@ -5,7 +5,7 @@
  *
  * Author   :  Gary Ash <gary.ash@icloud.com>
  * Created  :  10-Feb-2026  3:00pm
- * Modified :  22-Feb-2026
+ * Modified :  23-Feb-2026
  *
  * Copyright © 2026 By Gary Ash All rights reserved.
  ****************************************************************************************/
@@ -20,9 +20,15 @@ public enum UptimeInfo {
         let hours = (totalMinutes % (60 * 24)) / 60
         let minutes = totalMinutes % 60
 
-        let dayLabel = days == 1 ? "day" : "days"
         let hourLabel = hours == 1 ? "hour" : "hours"
         let minuteLabel = minutes == 1 ? "minute" : "minutes"
-        return .uptime("\(days) \(dayLabel), \(hours) \(hourLabel) \(minutes) \(minuteLabel)")
+        let timeString: String
+        if days > 0 {
+            let dayLabel = days == 1 ? "day" : "days"
+            timeString = "\(days) \(dayLabel), \(hours) \(hourLabel) \(minutes) \(minuteLabel)"
+        } else {
+            timeString = "\(hours) \(hourLabel) \(minutes) \(minuteLabel)"
+        }
+        return .uptime(timeString)
     }
 }
